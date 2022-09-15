e035daf3deee9d2ee54378ed64a94a7fc580fad0
Batuhan Erol <bataer57@gmail.com>
2022-09-15 16:48:58 +0300

    validation added

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
@@ -7,20 +9,39 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 })
 export class ProductAddComponent implements OnInit {
 
-  productAddForm : FormGroup
-  constructor(private formBuilder:FormBuilder) { }
+  productAddForm: FormGroup
+  constructor(private formBuilder: FormBuilder,
+    private productService: ProductService,
+    private toastrService: ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
   }
 
-  createProductAddForm(){
+  createProductAddForm() {
     this.productAddForm = this.formBuilder.group({
-      productName : ["",Validators.required],
-      unitPrice : ["",Validators.required],
-      unitsInStock : ["",Validators.required],
-      categoryId : ["",Validators.required]
+      productName: ["", Validators.required],
+      unitPrice: ["", Validators.required],
+      unitsInStock: ["", Validators.required],
+      categoryId: ["", Validators.required]
     })
   }
+  add() {
+    if (this.productAddForm.valid) {
+      let productModel = Object.assign({}, this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response => {
+        console.log(response)
+        this.toastrService.success(response.message, "Başarılı")
+      }, responseError => {
+        if (responseError.error.Errors.length > 0) {
+          for (let index = 0; index < responseError.error.Errors.length; index++) {
+            this.toastrService.error(responseError.error.Errors[index].ErrorMessage, "Doğrulama Hatası")
+          }
+        }
+      })
 
+    } else {
+      this.toastrService.error("Formunuz eksik", "Dikkat")
+    }
+  }
 }
